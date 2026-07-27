450f67cc961ea11187bc5899533263cad3323b68
Lalan Kumar <lkstudiokatoriya@gmail.com>
2026-07-27 19:30:20 +0530

    Add service worker for caching and offline functionality
    Implement service worker for caching files and offline support.

---
+++ sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = "python-learning-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/system_intro.html",
+  "/home.html",
+  "/manifest.json",
+
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+
+  "/offline.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys().then(keys => {
+
+            return Promise.all(
+                keys.map(key => {
+
+                    if(key !== CACHE_NAME){
+                        return caches.delete(key);
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            if(response){
+                return response;
+            }
+
+            return fetch(event.request)
+            .catch(() => {
+
+                return caches.match("/offline.html");
+
+            });
+
+        })
+
+    );
+
+});
