7202f744a02c3a0d72b923014f251b60ef4a3edf
Lalan Kumar <lkstudiokatoriya@gmail.com>
2026-09-02 16:51:38 +0530

    Create 03_variables.py

---
+++ Unit-01-Fundamentals/03_variables.py
@@ -0,0 +1,53 @@
+# 03_variables.py
+# Python Variables - Basic Examples
+
+# 1. Creating variables
+name = "Lalan"
+age = 18
+marks = 85.5
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+
+
+# 2. Different types of values
+student_name = "Rahul"
+student_age = 20
+student_marks = 78.5
+is_student = True
+
+print("\nStudent Details:")
+print(student_name)
+print(student_age)
+print(student_marks)
+print(is_student)
+
+
+# 3. Changing the value of a variable
+x = 10
+print("\nBefore:", x)
+
+x = 20
+print("After:", x)
+
+
+# 4. Multiple variables
+a, b, c = 10, 20, 30
+
+print("\nMultiple Variables:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+
+# 5. Same value in multiple variables
+p = q = r = 100
+
+print("\nSame Value:")
+print(p, q, r)
+
+
+# 6. Checking variable type
+number = 50
+print("\nType of number:", type(number))
