1e6cf43bf547188db8132540e7f07a80b1931f05
Lalan Kumar <lkstudiokatoriya@gmail.com>
2026-09-02 16:54:18 +0530

    Create 07_relational_operators.py

---
+++ Unit-01-Fundamentals/07_relational_operators.py
@@ -0,0 +1,25 @@
+# 07_relational_operators.py
+# Python Relational Operators
+
+a = 10
+b = 5
+
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b :", a > b)
+print("a < b :", a < b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
+
+
+# Taking input from user
+x = int(input("\nEnter first number: "))
+y = int(input("Enter second number: "))
+
+print("\nResults:")
+print("x == y:", x == y)
+print("x != y:", x != y)
+print("x > y :", x > y)
+print("x < y :", x < y)
+print("x >= y:", x >= y)
+print("x <= y:", x <= y)
