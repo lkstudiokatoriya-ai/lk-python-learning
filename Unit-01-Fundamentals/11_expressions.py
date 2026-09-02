c24d0a573951272655768b75cab678bd577f9326
Lalan Kumar <lkstudiokatoriya@gmail.com>
2026-09-02 16:56:56 +0530

    Create 11_expressions.py

---
+++ Unit-01-Fundamentals/11_expressions.py
@@ -0,0 +1,32 @@
+# 11_expressions.py
+# Python Expressions
+
+# 1. Arithmetic Expression
+a = 10
+b = 5
+result = a + b * 2
+print("Arithmetic Expression:", result)
+
+# 2. Relational Expression
+x = 10
+y = 20
+result = x < y
+print("Relational Expression:", result)
+
+# 3. Logical Expression
+p = 10
+q = 5
+result = p > 5 and q < 10
+print("Logical Expression:", result)
+
+# 4. Expression with Multiple Operators
+a = 20
+b = 10
+c = 5
+
+result = a + b * c
+print("Multiple Operators:", result)
+
+# 5. Expression using Parentheses
+result = (a + b) * c
+print("Using Parentheses:", result)
