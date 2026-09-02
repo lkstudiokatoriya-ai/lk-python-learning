391a342f98ecfa3306ada7b666e065473de6d560
Lalan Kumar <lkstudiokatoriya@gmail.com>
2026-09-02 16:56:12 +0530

    Create 10_type_conversion.py

---
+++ Unit-01-Fundamentals/10_type_conversion.py
@@ -0,0 +1,35 @@
+# 10_type_conversion.py
+# Python Type Conversion
+
+# 1. Integer to Float
+a = 10
+b = float(a)
+print("Integer to Float:", b)
+
+# 2. Float to Integer
+x = 10.5
+y = int(x)
+print("Float to Integer:", y)
+
+# 3. Integer to String
+num = 100
+text = str(num)
+print("Integer to String:", text)
+
+# 4. String to Integer
+value = "25"
+number = int(value)
+print("String to Integer:", number)
+
+# 5. String to Float
+value = "12.5"
+number = float(value)
+print("String to Float:", number)
+
+# 6. Integer to Boolean
+num = 1
+print("Integer to Boolean:", bool(num))
+
+# 7. Float to Integer using input
+marks = float(input("\nEnter marks: "))
+print("Integer value:", int(marks))
