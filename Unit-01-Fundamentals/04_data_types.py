67b983a358d672a9593bb88c5881817e75394ec6
Lalan Kumar <lkstudiokatoriya@gmail.com>
2026-09-02 16:52:15 +0530

    Create 04_data_types.py

---
+++ Unit-01-Fundamentals/04_data_types.py
@@ -0,0 +1,60 @@
+# 04_data_types.py
+# Python Data Types - Basic Examples
+
+# 1. Integer (int)
+age = 18
+print("Integer:", age)
+print("Type:", type(age))
+
+
+# 2. Float (float)
+marks = 85.5
+print("\nFloat:", marks)
+print("Type:", type(marks))
+
+
+# 3. Complex (complex)
+number = 3 + 4j
+print("\nComplex:", number)
+print("Type:", type(number))
+
+
+# 4. Boolean (bool)
+is_student = True
+print("\nBoolean:", is_student)
+print("Type:", type(is_student))
+
+
+# 5. String (str)
+name = "Lalan Kumar"
+print("\nString:", name)
+print("Type:", type(name))
+
+
+# 6. List
+subjects = ["Python", "Math", "Electrical"]
+print("\nList:", subjects)
+print("Type:", type(subjects))
+
+
+# 7. Tuple
+coordinates = (10, 20, 30)
+print("\nTuple:", coordinates)
+print("Type:", type(coordinates))
+
+
+# 8. None Type
+result = None
+print("\nNone:", result)
+print("Type:", type(result))
+
+
+# 9. Dictionary
+student = {
+    "name": "Lalan",
+    "age": 18,
+    "marks": 85
+}
+
+print("\nDictionary:", student)
+print("Type:", type(student))
