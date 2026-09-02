e706d7a8ff68f0ca3f661d751749f2bced95ed87
Lalan Kumar <lkstudiokatoriya@gmail.com>
2026-09-02 16:58:17 +0530

    Create 13_area_of_triangle.py

---
+++ Unit-01-Fundamentals/13_area_of_triangle.py
@@ -0,0 +1,9 @@
+# 13_area_of_triangle.py
+# Program to Calculate Area of Triangle
+
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+
+area = 0.5 * base * height
+
+print("Area of Triangle =", area)
